c27a083ddc7be58281d99b811876a5431bfd5725
Shimizu Yuta <shimizy35@gmail.com>
2018-09-20 17:00:29 +0900

    add Notes
    ノーツ描画用のクラスを作成/仮描画

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,15 +1,20 @@
 
 # include <Siv3D.hpp> // OpenSiv3D v0.2.8
-#include"Lane.h"
+#include"LaneBack.hpp"
+#include"Notes.hpp"
 
 void Main() {
     Window::Resize(1920, 1080);
     Window::SetTitle(U"MusicGame");
     Graphics::SetBackground(Palette::Yellow);
 
-    Lane::create();
+    Lane::LaneBack::create();
+    
+    Notes testNote(1,0);
     
     while (System::Update()) {
-        Lane::getInstance().draw();
+        Lane::LaneBack::getInstance().draw();
+        testNote.update(0);
+        testNote.draw();
     }
 }
