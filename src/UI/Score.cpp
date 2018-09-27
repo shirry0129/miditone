488fd08d7e90e9e7618b662e1beb4b936bd3f780
Shimizu Yuta <shimizy35@gmail.com>
2018-09-27 17:27:56 +0900

    add UI/Score

---
+++ src/UI/Score.cpp
@@ -0,0 +1,8 @@
+//
+//  Score.cpp
+//  MusicGame
+//
+//  Created by Shimizu Yuta on 2018/09/27.
+//
+
+#include "Score.hpp"
---
+++ src/UI/Score.hpp
@@ -0,0 +1,13 @@
+//
+//  Score.hpp
+//  MusicGame
+//
+//  Created by Shimizu Yuta on 2018/09/27.
+//
+
+#ifndef Score_hpp
+#define Score_hpp
+
+#include <stdio.h>
+
+#endif /* Score_hpp */
