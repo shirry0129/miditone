62c2c568dd654431454fa103b4d512becbf13c00
Shimizu Yuta <shimizy35@gmail.com>
2018-09-28 18:25:08 +0900

    add Score
    Scoreクラスの追加

--- src/UI/Score.cpp
+++ src/UI/Score.cpp
@@ -6,3 +6,52 @@
 //
 
 #include "Score.hpp"
+
+
+namespace ui {
+    
+    Score::Score(std::vector<score::Note>& _fromFile, float _speed) {
+        speed = _speed;
+        
+        for(auto n: _fromFile){
+            switch (n.type) {
+                case score::NoteType::HIT:
+                    score.emplace_back(n.lane, n.t_beg.sec);
+                    break;
+                    
+                case score::NoteType::HOLD:
+                    score.emplace_back(n.lane, n.t_beg.sec, n.t_end.sec);
+                    break;
+            }
+        }
+    }
+    
+    void Score::setFromFile(std::vector<score::Note>& _fromFile, float _speed) {
+        speed = _speed;
+        
+        for(auto n:_fromFile){
+            switch (n.type) {
+                case score::NoteType::HIT:
+                    score.emplace_back(n.lane, n.t_beg.sec);
+                    break;
+                    
+                case score::NoteType::HOLD:
+                    score.emplace_back(n.lane, n.t_beg.sec, n.t_end.sec);
+                    break;
+            }
+        }
+    }
+    
+    void Score::update(double currentTime) {
+        for(auto n:score){
+            n.update(currentTime, speed);
+        }
+    }
+    
+    void Score::draw() {
+        for (auto n:score) {
+            n.draw();
+        }
+    }
+    
+}
--- src/UI/Score.hpp
+++ src/UI/Score.hpp
@@ -8,6 +8,28 @@
 #ifndef Score_hpp
 #define Score_hpp
 
-#include <stdio.h>
+#include <Siv3D.hpp>
+#include <vector>
+#include "Note.hpp"
+#include "../System/Score.hpp"
+
+namespace ui {
+    
+    class Score {
+    private:
+        Array<Note> score;
+        float speed;
+        
+    public:
+        Score(){};
+        ~Score() = default;
+        Score(std::vector<score::Note>& _fromFile, float _speed);
+        void setFromFile(std::vector<score::Note>& _fromFile, float _speed);
+        
+        void update(double currentTime);
+        void draw();
+    };
+    
+}
 
 #endif /* Score_hpp */
