f43658eea1ae86640695ea9ea0ed3856107240c4
Shimizu Yuta <shimizy35@gmail.com>
2018-10-01 18:06:58 +0900

    add EventTimer
    EventTimer(from Siv3D)を追加
    再生開始前に一小節分の時間を追加

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,25 +4,41 @@
 #include"UI/Note.hpp"
 #include "System/ScoreManager.hpp"
 #include "UI/Score.hpp"
+#include "System/EventTimer.hpp"
 
 void Main() {
     Window::Resize(1920, 1080);
     Window::SetTitle(U"MusicGame");
     Graphics::SetBackground(Palette::Yellow);
+//    Graphics::SetTargetFrameRateHz(120);
+//    Graphics::SetFullScreen(true, {1280, 720});
 
     ui::LaneBG::create();
     
-    score::ScoreManager testFile("../resource/438_0905.txt", score::Difficulty::EASY);
+    score::ScoreManager testFile("../resource/427_1001.txt", score::Difficulty::HARD);
+    double measureLength = testFile.getBar()[1].time.sec;
+
+    ui::Score testScore(testFile.getNotes(), 1.5);
+
+    const Audio testAudio(U"../resource/larva.mp3");
+
+//    testAudio.play();
     
-    ui::Score testScore(testFile.getNotes(), 1);
+//    Stopwatch time(true);
+    EventTimer time;
+    time.addEvent(U"Start", SecondsF(measureLength));
     
-    Stopwatch count(true);
+    time.start();
     
     while (System::Update()) {
+        time.update();
         ui::LaneBG::getInstance().draw();
         
-        testScore.update(count.sF());
-        testScore.draw();
+        if (time.onTriggered(U"Start")) {
+            testAudio.play();
+        }
         
+        testScore.update(time.sF() - measureLength);
+        testScore.draw();
     }
 }
