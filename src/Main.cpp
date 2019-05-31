06945cb1ed5dda45e49bfd6d66c16aeeba5791bb
Shimizu Yuta <shimizy35@gmail.com>
2019-05-31 23:32:45 +0900

    fix TextureAssets
    コード修正無しでWin/macのビルドが通るように変更

--- src/Main.cpp
+++ src/Main.cpp
@@ -51,6 +51,8 @@ void Main() {
     FontAsset::Register(U"totalTrackNum", 30);
     FontAsset::Register(U"totalSong", 45, Typeface::Bold);
     FontAsset::Register(U"totalArtist", 30);
+    
+#if defined(SIV3D_TARGET_MACOS)
     TextureAsset::Register(U"gameStart", Resource(U"resource/gamestart.png"), TextureDesc::Mipped);
     TextureAsset::Register(U"boxTemplate", Resource(U"resource/box.png"), TextureDesc::Mipped);
     TextureAsset::Register(U"title", Resource(U"resource/title.jpg"));
@@ -69,6 +71,26 @@ void Main() {
     TextureAsset::Register(U"instBack", Resource(U"resource/back.png"));
     TextureAsset::Register(U"resultDecision", Resource(U"resource/result_box.png"));
     TextureAsset::Register(U"resultCombo", Resource(U"resource/max_combo_.png"));
+#elif defined(SIV3D_TARGET_WINDOWS)
+    TextureAsset::Register(U"gameStart", U"resource/gamestart.png", TextureDesc::Mipped);
+    TextureAsset::Register(U"boxTemplate", U"resource/box.png", TextureDesc::Mipped);
+    TextureAsset::Register(U"title", U"resource/title.jpg");
+    TextureAsset::Register(U"select", U"resource/select.jpg");
+    TextureAsset::Register(U"preference", U"resource/preference.jpg");
+    TextureAsset::Register(U"play", U"resource/play.jpg");
+    TextureAsset::Register(U"result", U"resource/result.jpg");
+    TextureAsset::Register(U"totalResult", U"resource/totalresult.jpg");
+    TextureAsset::Register(U"track", U"resource/track.png");
+    TextureAsset::Register(U"score", U"resource/score_box.png", TextureDesc::Mipped);
+    TextureAsset::Register(U"song", U"resource/song_box_long.png", TextureDesc::Mipped);
+    TextureAsset::Register(U"gameover", U"resource/end.png");
+    TextureAsset::Register(U"hitNote", U"resource/hitnote.png");
+    TextureAsset::Register(U"arrow", U"resource/ya.png");
+    TextureAsset::Register(U"return", U"resource/turn.png");
+    TextureAsset::Register(U"instBack", U"resource/back.png");
+    TextureAsset::Register(U"resultDecision", U"resource/result_box.png");
+    TextureAsset::Register(U"resultCombo", U"resource/max_combo_.png");
+#endif
     
     ui::MyApp sceneManager;
     sceneManager.setFadeColor(gameinfo::fadeColor);
